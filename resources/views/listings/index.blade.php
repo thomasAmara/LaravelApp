07ccc8a855fe8a25b3ec28eb5fef2931288a96b1
thomasAmara <thomastarawali@gmail.com>
2022-06-18 12:07:18 +0100

    code refactor

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -5,17 +5,21 @@
     @include('partials._search')
 
     {{-- <h1>{{ $heading }}</h1> --}}
-    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
+    <div class="lg:grid lg:grid-cols-2 gap-4 space-y-8 md:space-y-0 mx-4">
 
         @unless(count($listings) == 0)
-            @if (count($listings) == 0)
-                <p>No listing found</p>
-            @endif
 
             @foreach ($listings as $listing)
                 <x-listing-card :listing='$listing' />
             @endforeach
+        @else
+            <p>No listing found</p>
         @endunless
+
+    </div>
+
+    <div class="mt-6 p-4">
+        {{-- {{ $listings->links() }} --}}
     </div>
 
 </x-layout>
