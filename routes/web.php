72a6661b08ed006327ef5f38d919d0f38a85953f
thomasAmara <thomastarawali@gmail.com>
2022-06-18 16:49:17 +0100

    route path for the listing store and create

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,16 @@ use App\Models\Listing;
 // All Listings
 Route::get('/', [ListingController::class, 'index']);
 
+//Show create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
+
+
+
+
+
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
