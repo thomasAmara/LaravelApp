7649c0033fea85236dd328afe91468be9c7f5ffb
thomasAmara <thomastarawali@gmail.com>
2022-06-18 16:47:59 +0100

    Listing controller to Create listing with Rule

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
@@ -17,10 +18,33 @@ class ListingController extends Controller
         ]);
     }
 
-    //show single listing
-    public function show(Listing $listing) {
+      //show single listing
+      public function show(Listing $listing) {
         return view('listings.show', [
             'listing' => $listing
         ]);
+    }
+
+     //show create Form
+     public function create(Listing $listing) {
+        return view('listings.create', [
+            'listing' => $listing
+        ]);
+    }
+
+       //store Listing Data
+       public function store(Request $request) {
+            $formFields = $request->validate([
+            'title' => 'required',
+            'tags' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'email' => ['required', 'email'],
+            'website' => 'required',           
+            'description' => 'required',
+      ]);
+
+      Listing::create($formFields); //to create the database
+      return redirect('/')->with('message', 'Listing Created successfully!');
     }
 }
