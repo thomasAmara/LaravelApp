4ad2fd413617d4c8959553cea47442514f7591b1
thomasAmara <thomastarawali@gmail.com>
2022-06-18 16:54:09 +0100

    filter search

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -9,16 +9,18 @@ class Listing extends Model
 {
     use HasFactory;
 
+    // protected $fillable = [ 'title', 'company', 'location', 'website','email', 'description', 'tags'];
+
     public function scopeFilter($query, array $filters) {
         
         if ($filters['tag'] ?? false) {
-            $query->where('tags', 'like', ' %' . request('tag') . '%' );
+            $query->where('tags', 'like', '%' . request('tag') . '%' );
         }
 
         if ($filters['search'] ?? false) {
-            $query->where('title', 'like', ' %' . request('search') . '%' )
-            -> orWhere( 'description', 'like', ' %' . request('search') . '%' )
-            -> orWhere( 'tags', 'like', ' %' . request('search') . '%' );
+            $query->where('title', 'like', '%' . request('search') . '%' )
+            -> orWhere( 'description', 'like', '%' . request('search') . '%' )
+            -> orWhere( 'tags', 'like', '%' . request('search') . '%' );
         }
     }
 }
