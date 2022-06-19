641ddd8baf44c17cd6f62421349ba61a72f90f38
thomasAmara <thomastarawali@gmail.com>
2022-06-19 17:18:15 +0100

    route for login and register

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,13 +27,14 @@
 
 <body class="mb-48">
     <nav class="flex justify-between items-center mb-4">
-        <a href="/"><img class="w-24" src="images/logo.png" alt="" class="logo" /></a>
+        <a href="/"><img class="w-24" src="{{ asset('images/logo.png') }}" alt=""
+                class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
             <li>
-                <a href="register.html" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+                <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
             </li>
             <li>
-                <a href="login.html" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
+                <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i>
                     Login</a>
             </li>
         </ul>
