8c3e1641297eb8d9c7df40d045867da477b0353b
thomasAmara <thomastarawali@gmail.com>
2022-06-19 17:21:17 +0100

    Logo file

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -7,11 +7,12 @@
             <p class="mb-4">Post a gig to find a developer</p>
         </header>
 
-        <form method="POST" action="/listings">
+        <form method="POST" action="/listings" enctype="multipart/form-data">
             @csrf
             <div class="mb-6">
                 <label for="company" class="inline-block text-lg mb-2">Company Name</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="company"
+                    value="{{ old('company') }}" />
                 @error('company')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -20,7 +21,7 @@
             <div class="mb-6">
                 <label for="title" class="inline-block text-lg mb-2">Job Title</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="title"
-                    placeholder="Example: Senior Laravel Developer" />
+                    placeholder="Example: Senior Laravel Developer" value="{{ old('title') }}" />
                 @error('title')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -29,7 +30,7 @@
             <div class="mb-6">
                 <label for="location" class="inline-block text-lg mb-2">Job Location</label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="location"
-                    placeholder="Example: Remote, Boston MA, etc" />
+                    placeholder="Example: Remote, Boston MA, etc" value="{{ old('location') }}" />
                 @error('location')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -37,7 +38,8 @@
 
             <div class="mb-6">
                 <label for="email" class="inline-block text-lg mb-2">Contact Email</label>
-                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email" />
+                <input type="text" class="border border-gray-200 rounded p-2 w-full" name="email"
+                    value="{{ old('email') }}" />
                 @error('email')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
@@ -58,25 +60,28 @@
                     Tags (Comma Separated)
                 </label>
                 <input type="text" class="border border-gray-200 rounded p-2 w-full" name="tags"
-                    placeholder="Example: Laravel, Backend, Postgres, etc" />
+                    placeholder="Example: Laravel, Backend, Postgres, etc" value="{{ old('tags') }}" />
                 @error('tags')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
             </div>
 
-            {{-- <div class="mb-6">
+            <div class="mb-6">
                 <label for="logo" class="inline-block text-lg mb-2">
                     Company Logo
                 </label>
                 <input type="file" class="border border-gray-200 rounded p-2 w-full" name="logo" />
-            </div> --}}
+                @error('logo')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                @enderror
+            </div>
 
             <div class="mb-6">
                 <label for="description" class="inline-block text-lg mb-2">
                     Job Description
                 </label>
                 <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10"
-                    placeholder="Include tasks, requirements, salary, etc"></textarea>
+                    placeholder="Include tasks, requirements, salary, etc" value="{{ old('description') }}"></textarea>
                 @error('description')
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                 @enderror
