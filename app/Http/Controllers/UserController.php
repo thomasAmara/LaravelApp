90c30dd2597b87a70d8b1d88c295d0e306d12c69
thomasAmara <thomastarawali@gmail.com>
2022-06-20 09:08:55 +0100

    user Auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     //show register/create Form
     public function create() {
         return view('users.register');
+    }
+
+    //Create new Users
+    public function store(Request $request) {
+    $formFields = $request->validate([
+        'name' => ['required', 'min:3'],
+        'email' => ['required', 'email', Rule::unique('users', 'email')],
+        'password' => 'required|confirmed|min:6'
+    ]);
+
+    //HASH Password
+    $formFields['password'] = bcrypt($formFields['password']);
+
+    //Create User
+    $user = User::create($formFields);
+
+    //Login
+    auth()->login($user);
+
+    return redirect('/')->with('message', 'User created and  logged in');
     }
 
     //show login Form
